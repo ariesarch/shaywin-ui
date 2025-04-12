e8b902b99eb87c38b385ebb3018e9de87d7e93b9
kyawyenaing <kyawyenaing.dev@gmail.com>
2025-04-13 02:43:29 +0900

    dist 9

--- src/types.d.ts
+++ src/types.d.ts
@@ -2,8 +2,12 @@ declare global {
   namespace JSX {
     interface IntrinsicElements {
       // Declare your custom elements here
+      's-button': any;
+      's-input': any;
       's-button': React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement>;
       's-input': React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement>;
+      's-button': any;
+      's-input': any;
     }
   }
 }
